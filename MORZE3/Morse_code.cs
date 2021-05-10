7d35ea2b2930c24e62aacb1121983d2dd52f5050
qfx111 <qfxs.pochta@gmail.com>
2021-05-10 17:54:49 +0300

    Add project files.

---
+++ MORZE3/Morse_code.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static Morse_code.Morse_Translator.Morse;
+
+namespace Morse_code
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Select Languages 1 ToEnglish 2 ToMorse 3 ToMorseFromRussian 4 ToMorseFormRussian");
+            var i = int.Parse(Console.ReadLine() ?? string.Empty);
+            Translate(i);
+        }
+    }
+}
